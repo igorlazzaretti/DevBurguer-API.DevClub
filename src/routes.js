f638a77e703193b2cfeafd5a6095a3f3bec1150b
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-12-27 22:58:52 -0300

    :man_technologist:API DevBurguer update: user controller with HTTpie. A14P1

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,10 @@
 // const { Router } = require('express');
 import { Router } from 'express';
-// UUID
-import { v4 } from 'uuid'
-
-import User from './app/models/User'
-import { password } from './config/database';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Harry Potter One',
-        email: 'hp@email.com',
-        password_hash: '12i2ji2i2h3u23bnndj#wsavada',
-        admin: true,
-    })
-    return res.status(201).json(user)
-})
+routes.post('/users', UserController.store)
 
 
 // module.exports = routes;
