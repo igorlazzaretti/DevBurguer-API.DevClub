d5932cb9a4edfcadb685f171d77828e21d3ff4de
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-12-27 23:47:28 -0300

    :man_technologist:API DevBurguer update: yup validation. A15P1

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,27 @@
 // UUID
 import { v4 } from 'uuid'
 
+import * as Yup from 'yup'
+
 import User from '../models/User'
 
 class UserController {
     async store(request, response) {
+
+        // Validando os dados
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean()
+        })
+
+        try {
+            await schema.validate(request.body, {abortEarly: false})
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
         const { name, email, password_hash, admin } = request.body
 
         const user = await User.create({
@@ -12,7 +29,7 @@ class UserController {
             name,
             email,
             password_hash,
-            admin,
+            admin
         })
         return response.status(201).json({
             id: user.id,
