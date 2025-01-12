e89293ac089c278156cb68f962d34563de2a9992
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2025-01-12 09:50:26 -0300

    :man_technologist:API DevBurguer Aula 21 Módulo 1 | Biblioteca Multer Upload de Imagem;

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 // const { Router } = require('express');
 import { Router } from 'express';
+// Multer: imagens
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
 
 const routes = new Router();
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
-routes.post('/products', ProductController.store)
+routes.post('/products', upload.single('file'), ProductController.store)
+
 
 
 // module.exports = routes;
